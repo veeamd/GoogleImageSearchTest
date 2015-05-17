86556a94531d642a521eee9e3f53f7aff5bca0f0
Wei Zhang <veeamd@gmail.com>
2015-05-17 13:28:34 -0400

    Initial Commit

---
+++ GoogleImageSearchTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoogleImageSearchTest
+//
+//  Created by Wei Zhang on 5/17/15.
+//  Copyright (c) 2015 weizhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoogleImageSearchTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoogleImageSearchTest
+//
+//  Created by Wei Zhang on 5/17/15.
+//  Copyright (c) 2015 weizhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
